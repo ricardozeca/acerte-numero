17aeed1b54a698ef6545fb5a256c006147390f1e
Ricardo Moraes <ricardo.fgm77@gmail.com>
2023-10-21 16:37:10 -0300

    refactor: Remoção console.log

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 alert ("Boas vindas ao jogo do número secreto.");
 let numeroMaximo = 100;
 let numeroSecreto = parseInt (Math.random() * numeroMaximo + 1);
-console.log (numeroSecreto);
 let chute;
 let tentativas = 1;
 
