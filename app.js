aedf0eb9e45963e88944a3284edf4c573be60838
Ricardo Moraes <ricardo.fgm77@gmail.com>
2023-10-20 21:36:01 -0300

    feat:Finalizando o jogo com números de tentativas maior e sendo ele aleatório.

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 alert ("Boas vindas ao jogo do número secreto.");
-let numeroSecreto = 13;
-console.log (numeroSecreto)
-let chute 
+let numeroMaximo = 100;
+let numeroSecreto = parseInt (Math.random() * numeroMaximo + 1);
+console.log (numeroSecreto);
+let chute;
+let tentativas = 1;
 
 // enquanto chute não for igual ao numero secreto
 while(chute != numeroSecreto){
-    chute = prompt ("Escolha um numero entre 1 ou 50");
+    chute = prompt (`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual o número secreto
-    if (chute == numeroSecreto){
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    if (chute == numeroSecreto) {
+        break;
+        
     } else {
-        alert("ERRROUUU");
         if(chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}` )
+            alert(`O número secreto é maior que ${chute}` );
         }
+        tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+//if (tentativas > 1) {
+//alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
+
